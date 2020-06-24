632c6b36e0e7aa0a105d1235efd10d0fcbbffae0
Felipe Andrade <47376654+felo77@users.noreply.github.com>
2020-06-23 23:48:57 -0500

    Validacion de usuario y contraseña

---
+++ app/src/main/java/com/andradefelipe/contactoappandradefelipe/LoginActivity.kt
@@ -0,0 +1,35 @@
+package com.andradefelipe.contactoappandradefelipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        button_login.setOnClickListener {
+
+            var user = editTextTextEmailAddress.getText().toString()
+            var psw = editTextTextPassword.getText().toString()
+
+            if (psw.length >= 8 && !user.isEmpty() && !psw.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(
+                    user
+                ).matches()
+            ) {
+                Toast.makeText(this, "Bienvenido a tus contactos", Toast.LENGTH_SHORT).show()
+            }
+            if (!Patterns.EMAIL_ADDRESS.matcher(user).matches() || user.isEmpty()) {
+                editTextTextEmailAddress.setError("Correo no valido, intente de nuevo")
+            }
+            if (psw.isEmpty() || psw.length < 8) {
+                editTextTextPassword.setError("Contraseña no valida, intente de nuevo")
+            }
+        }
+    }
+}
